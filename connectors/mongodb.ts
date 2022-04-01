4fbb69f64c4ff9b08539feba774abfb064752558
Cloud Yu <j.cloud.yu@gmail.com>
2022-04-02 02:52:24 +0800

    1. Add connection checking logic 2. Allow developers to initialize a MongoConnector using a client directly. 3. Correct dependent mongodb version

--- connectors/mongodb.ts
+++ connectors/mongodb.ts
@@ -1,32 +1,41 @@
 import type {IStorageConnect, StoredTypes, AllowedInputTypes} from "../varstore.js";
 import mongodb = require("mongodb");
 
-type InitOptions = { uri:string; db?:string; collection:string; };
+type InitOptions = { connection:mongodb.MongoClient; uri:string; db?:string; collection:string; };
 type VarRecord<ValueType> = {name:string; value:ValueType;}
+type MongoConnectorPrivate = {conn:mongodb.MongoClient; db:mongodb.Db; coll_name:string;};
 
 
-
-const _MongoConnector:WeakMap<MongoConnector, {conn:mongodb.MongoClient; db:mongodb.Db; coll_name:string;}> = new WeakMap();
+const _MongoConnector:WeakMap<MongoConnector, MongoConnectorPrivate> = new WeakMap();
 class MongoConnector implements IStorageConnect {
 	static async init(conn_info:InitOptions):Promise<MongoConnector> {
-		const raw_uri = (''+(conn_info.uri||'')).trim();
+		let connection:mongodb.MongoClient;
+		
+		if ( conn_info.connection ) {
+			connection = conn_info.connection;
+		}
+		else {
+			const raw_uri = (''+(conn_info.uri||'')).trim();
+			connection = await mongodb.MongoClient.connect(raw_uri);
+		}
+
+		
 
 		const db_name = conn_info.db;
 		const coll_name = (''+(conn_info.collection||'')).trim();
 		if ( !coll_name ) throw new SyntaxError("Bound collection name is required!");
 
-		const mongo_client = await mongodb.MongoClient.connect(raw_uri);
-		const mongo_db = db_name ? mongo_client.db() : mongo_client.db(db_name);
-
 		const instance = new MongoConnector();
 		_MongoConnector.set(instance, {
 			coll_name,
-			conn:mongo_client,
-			db:mongo_db
+			conn:connection,
+			db:db_name ? connection.db() : connection.db(db_name)
 		});
 		return instance;
 	}
 	async list():Promise<string[]> {
+		CheckConn(this);
+
 		const {db, coll_name} = _MongoConnector.get(this)!;
 		const [result] = await db.collection(coll_name).aggregate<{names:string[]}>([
 			{$sort:{name:1}},
@@ -37,8 +46,10 @@ class MongoConnector implements IStorageConnect {
 		return result ? result.names : [];
 	}
 	async get(name:string):Promise<StoredTypes|undefined> {
+		CheckConn(this);
+		
 		const {db, coll_name} = _MongoConnector.get(this)!;
-		const [data] = await db.collection(coll_name).find<VarRecord<StoredTypes>>({name}).toArray();
+		const [data] = await db.collection<VarRecord<StoredTypes>>(coll_name).find({name}).toArray();
 		if ( !data ) return undefined;
 		if ( data.value instanceof mongodb.Binary ) {
 			return data.value.buffer;
@@ -46,6 +57,8 @@ class MongoConnector implements IStorageConnect {
 		return data.value;
 	}
 	async set(name:string, value:AllowedInputTypes):Promise<boolean> {
+		CheckConn(this);
+
 		const {db, coll_name} = _MongoConnector.get(this)!;
 		if ( Buffer.isBuffer(value) || value instanceof ArrayBuffer ) {
 			value = Buffer.from(value);
@@ -59,6 +72,8 @@ class MongoConnector implements IStorageConnect {
 		return (result.upsertedCount + result.matchedCount) > 0;
 	}
 	async del(name:string):Promise<StoredTypes|undefined> {
+		CheckConn(this);
+		
 		const {db, coll_name} = _MongoConnector.get(this)!;
 		const {value} = await db.collection(coll_name).findOneAndDelete({name});
 		if ( value === null ) return undefined;
@@ -68,6 +83,13 @@ class MongoConnector implements IStorageConnect {
 	async release() {
 		const {conn} = _MongoConnector.get(this)!;
 		return conn.close();
+	}
+}
+
+function CheckConn(inst:MongoConnector) {
+	const {conn} = _MongoConnector.get(inst)!;
+	if ( !conn.isConnected() ) {
+		throw new Error("Database connection has been lost!");
 	}
 }
 
